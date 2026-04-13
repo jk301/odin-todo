d1ab4365ba06c829d51c0135d48efd510f66cbb8
junaid <jk7948677@gmail.com>
2026-04-13 22:52:08 +0530

    Added header, card modules (no functionality)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 /* index.js */
 import "./styles.css";
 import { sidebar } from "./sidebar/sidebar.js";
+import { main_header, main_content } from "./main_page/main_page.js";
 
 const sidebarContent = sidebar;
+const mainHeader = main_header;
+const mainContent = main_content;
+
 document.body.append(sidebarContent.container);
+
+const mainBody = document.createElement("div");
+mainBody.classList.add("main-body");
+document.body.appendChild(mainBody);
+
+mainBody.appendChild(mainHeader.container);
+mainBody.appendChild(mainContent.container);
+
+mainContent.createCard("title", "description", "due", "priority");
+mainContent.createCard("title", "description", "due", "priority");
+mainContent.createCard("title", "description", "due", "priority");
+mainContent.createCard("title", "description this is a long one that might break the link barrier of the card", "due", "priority");
+mainContent.createCard("title", "description", "due", "priority");
+mainContent.createCard("title", "description", "due", "priority");
+mainContent.createCard("title", "description", "due", "priority");
+mainContent.createCard("title", "description", "due", "priority");
 
--- src/main_page/main_page.js
+++ src/main_page/main_page.js
@@ -1,1 +1,78 @@
 /* main_page */
+import "./main_page.css"
+import doneIcon from "../icons/done.svg";
+import editIcon from "../icons/edit.svg";
+import addIcon from "../icons/add.svg";
+
+export const main_header = (() => {
+    const header = document.createElement("div");
+    header.classList.add("header");
+
+    const main_title = document.createElement("h1");
+    const main_desc = document.createElement("p");
+
+    function setContent(title, desc) {
+        main_title.textContent = title;
+        main_desc.textContent = desc;
+    }
+    setContent("example title", "example description yes")
+
+    header.appendChild(main_title);
+    header.appendChild(main_desc);
+
+    return { container : header, setContent };
+
+})();
+
+export const main_content = (() => {
+    const content = document.createElement("div");
+    content.classList.add("content");
+
+    function createCard(title, desc, due, priority) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        // text content wrapper
+        const cardText = document.createElement("div");
+        cardText.classList.add("card-text");
+
+        const cardTitle = document.createElement("h2");
+        cardTitle.textContent = title;
+
+        const cardDesc = document.createElement("p");
+        cardDesc.textContent = `Description : ${desc}`;
+
+        const cardDue = document.createElement("p");
+        cardDue.textContent = `Due : ${due}`;
+
+        cardText.appendChild(cardTitle);
+        cardText.appendChild(cardDesc);
+        cardText.appendChild(cardDue);
+
+        // icons wrapper
+        const cardIcons = document.createElement("div");
+        cardIcons.classList.add("card-icons");
+
+        const cardCheck = document.createElement("img");
+        cardCheck.src = doneIcon;
+
+        const cardEdit = document.createElement("img");
+        cardEdit.src = editIcon;
+
+        cardIcons.appendChild(cardEdit);
+        cardIcons.appendChild(cardCheck);
+        
+        card.appendChild(cardText);
+        card.appendChild(cardIcons);
+
+        content.appendChild(card);
+    }
+
+    const addBtn = document.createElement("img");
+    addBtn.src = addIcon;
+    addBtn.classList.add("add-card-icon");
+    addBtn.title = "Add ToDo";
+    content.appendChild(addBtn);
+
+    return { container : content, createCard }
+})();
